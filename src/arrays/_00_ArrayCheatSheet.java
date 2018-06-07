dc828ffb1ca67b32dbf0f3cf182488402d138d4f
yuandrew2014 <league@WTS22.attlocal.net>
2018-06-06 19:58:28 -0700

    almost done with array lists project

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -1,27 +1,45 @@
 package arrays;
 
+import java.util.Random;
+
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
-		//1. make an array of 5 Strings
-		
-		//2. print the third element in the array
-		
-		//3. set the third element to a different value
-		
-		//4. print the third element again
-		
-		//5. use a for loop to print all the values in the array
-		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
-		
-		//6. make an array of 50 integers
-		
-		//7. use a for loop to make every value of the integer array a random number
-		
-		//8. without printing the entire array, print only the smallest number in the array
-		
-		//9 print the entire array to see if step 8 was correct
-		
-		//10. print the largest number in the array.
+		// 1. make an array of 5 Strings
+
+		// String[] five = new String[5];
+		Random rand = new Random();
+		int a = rand.nextInt(47);
+		// 2. print the third element in the array
+		// System.out.println(five[3]);
+		// 3. set the third element to a different value
+		// five[3] = "3";
+		// 4. print the third element again
+		// System.out.println(five[3]);
+		// 5. use a for loop to print all the values in the array
+		// for (int i = 0; i < five.length; i++) {
+		// System.out.println(five[i]);
+		// }
+		// BE SURE TO USE THE ARRAY'S length VARIABLE
+
+		// 6. make an array of 50 integers
+		int[] fifty = new int[50];
+		int smallest = fifty[0];
+		// 7. use a for loop to make every value of the integer array a random number
+		for (int i = 0; i < fifty.length; i++) {
+			fifty[i] = rand.nextInt(47);
+		}
+		// 8. without printing the entire array, print only the smallest number in the
+		// array
+		for (int i = 0; i < fifty.length; i++) {
+			if (smallest > fifty[i]) {
+				smallest = fifty[i];
+
+			}
+
+		}
+		System.out.println(smallest);
+		// 9 print the entire array to see if step 8 was correct
+
+		// 10. print the largest number in the array.
 	}
 }
